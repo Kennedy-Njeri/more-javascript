8ba98fa4139b9721581e63f5c9afb558b35e7aa4
Kennedy Njeri <mistakenz123@gmail.com>
2020-03-14 23:46:54 +0300

    clean up

--- explore/callbacks.js
+++ explore/callbacks.js
@@ -11,19 +11,19 @@
 // console.log(shortNames)
 
 
-const geoCode = (address, callback) => {
-    setTimeout(() => {
-        const data = {
-            latitude: 0,
-            longitude: 0
-        }
-        callback(data)
-    }, 2000)
-}
-
-geoCode('Nairobi', (data) => {
-    console.log(data)
-})
+// const geoCode = (address, callback) => {
+//     setTimeout(() => {
+//         const data = {
+//             latitude: 0,
+//             longitude: 0
+//         }
+//         callback(data)
+//     }, 2000)
+// }
+//
+// geoCode('Nairobi', (data) => {
+//     console.log(data)
+// })
 
 
 const numbers = (a, b, callback) => {
